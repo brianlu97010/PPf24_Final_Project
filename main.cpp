73270c630a69c8f2b96e435476fe6acd62ccf1a5
Brian <brianlu97010@gmail.com>
2024-11-29 20:32:24 +0800

    Add the feature can specify the output file name

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@
 //-------------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
-	if(argc<2)
-	{
-		printf("Usage: %s inputFile\n\tInput file must be 24bit bitmap file.\n", argv[0]);
-		return 1;
-	}
+    if(argc < 3)
+    {
+        printf("Usage: %s {inputFile} {outputFile}\n\tInput file must be 24bit bitmap file.\n", argv[0]);
+        return 1;
+    }
 
-	const char* inputFileName = argv[1];
+    const char* inputFileName = argv[1];
+    const char* outputFileName = argv[2];
 
-	JpegEncoder encoder;
-	if(!encoder.readFromBMP(inputFileName))
-	{
-		return 1;
-	}
+    JpegEncoder encoder;
+    if(!encoder.readFromBMP(inputFileName))
+    {
+        return 1;
+    }
 
-	if(!encoder.encodeToJPG("out.jpg", 50))
-	{
-		return 1;
-	}
-	return 0;
+    if(!encoder.encodeToJPG(outputFileName, 50))
+    {
+        return 1;
+    }
+    return 0;
 }
