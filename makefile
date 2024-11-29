43b6cda7fb2398d7f3880dd9f70a1eef465cadd0
G8030b-nycu <g8030b.cs13@nycu.edu.tw>
2024-11-30 05:13:10 +0800

    upload CUDA

---
+++ makefile
@@ -0,0 +1,19 @@
+CXX = nvcc
+CXXFLAGS = -std=c++17 -O3
+
+TARGET = jpeg
+
+SRC = test.cu jpeg_encoder.cu
+
+OBJ = $(SRC:.cu=.o)
+
+all: $(TARGET)
+
+$(TARGET) : $(OBJ)
+	$(CXX) $(CXXFLAGS) -o $(TARGET) $(OBJ)
+
+%.o: %.cu
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ) $(TARGET)
---
+++ test.cu
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "jpeg_encoder.h"
+#include "common/CycleTimer.h"
+
+//-------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+	if(argc<2)
+	{
+		printf("Usage: %s inputFile\n\tInput file must be 24bit bitmap file.\n", argv[0]);
+		return 1;
+	}
+
+	const char* inputFileName = argv[1];
+
+	JpegEncoder encoder;
+
+	if(!encoder.readFromBMP(inputFileName))
+	{
+		return 1;
+	}
+
+	double startTime = CycleTimer::currentSeconds();
+	if(!encoder.encodeToJPG("out.jpg", 50))
+	{
+		return 1;
+	}
+
+	double endTime = CycleTimer::currentSeconds();
+
+        printf("encodeToJPG execution time: %.3f seconds\n", (endTime - startTime));
+
+	return 0;
+}
